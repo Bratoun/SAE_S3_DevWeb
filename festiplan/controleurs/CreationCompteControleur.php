b756eabaae8ae950e892db2d0ed41f01bdf56f55
Nathan <nathanpaulgirardin@orange.fr>
2023-12-13 15:37:33 +0100

    correction

--- festiplan/controleurs/CreationCompteControleur.php
+++ festiplan/controleurs/CreationCompteControleur.php
@@ -7,24 +7,11 @@ use yasmf\View;
 
 class CreationCompteControleur
 {
-<<<<<<< HEAD
-    public function __construct() {
-=======
-    private UserModele $userModele;
 
-    public function __construct(UserModele $userModele) {
-        $this->userModele = $userModele;
->>>>>>> 0b1de11f5f59c08e654497175f62afea2e3db5ba
-    }
+    public function __construct() {
 
     public function index($pdo)
     {
-<<<<<<< HEAD
         return new View("vues/vueInscription");
-=======
-        // $login = HttpHelper::getParam("login");
-        // $mdp = HttpHelper::getParam("mdp");
-        // $searchStmt = $this->userModele->
->>>>>>> 0b1de11f5f59c08e654497175f62afea2e3db5ba
     }
 }
--- festiplan/vues/vueConnexion.php
+++ festiplan/vues/vueConnexion.php
@@ -30,19 +30,7 @@
                 </div>
                 <br><br>
             </form>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
             <form action="index.php" method="post">
-=======
-            <form action="vueInscription.php" method="post">
->>>>>>> 0b1de11f5f59c08e654497175f62afea2e3db5ba
-=======
-            <form action="vueInscription.php" method="post">
->>>>>>> 3c3b2ec96bf95963ef3e087fb30cd7754791d0ae
-=======
-            <form action="vueInscription.php" method="post">
->>>>>>> 3c3b2ec96bf95963ef3e087fb30cd7754791d0ae
                 <p class="texteCentre petit">Vous n'avez pas de compte ? <button class="btn petit">CREER UN COMPTE</button></p>
             </form>
         </div>
