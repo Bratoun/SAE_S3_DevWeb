38bceebf95ba7d70249714391e6bccbd62adae5a
Bratoun <rayan.ibrahime@iut-rodez.fr>
2023-12-13 15:59:42 +0100

    modification css

--- festiplan/vues/vue_accueil.php
+++ festiplan/vues/vue_accueil.php
@@ -7,13 +7,13 @@
     <link rel="stylesheet" href="static/css/index.css"/>
     <link href="static/fontawesome-free-6.2.1-web/css/all.min.css" rel="stylesheet">
 </head>
-<body>
-
-    <!--En tête-->
-    <header>
+<!--En tête-->
+<header>
         <div class="header"><i class="fa-solid fa-user"></i></div>
-    </header>
+</header>
+<body id="body-blanc">
 
+    
     
 
     <button type="submit" class="btn_creer_spectacle">Créer un spectacle</button>
